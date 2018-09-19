da5c39a5e6b4f9a0e14e6ad6dee1d556875ba43e
burghporter314 <burghporter314@gmail.com>
2018-09-18 20:03:10 -0400

    Updated Record Class

--- src/com/x10host/burghporter314/DatabaseManagementSystem/Record.java
+++ src/com/x10host/burghporter314/DatabaseManagementSystem/Record.java
@@ -1,5 +1,30 @@
 package com.x10host.burghporter314.DatabaseManagementSystem;
 
 public class Record {
-
+	
+	private Column[] columns;
+	private String[] values;
+	private int position = 0;
+	
+	public Record(Column[] columns) {
+		this.columns = columns;
+		this.values = new String[columns.length];
+	}
+	
+	public void addValue(String value) {
+		values[position++] = value;
+	}
+	
+	public String[] getValues() {
+		return this.values;
+	}
+	
+	public void setValues(String[] values) {
+		this.values = values;
+		this.position = values.length - 1;
+	}
+	
+	public boolean isFull() {
+		return ((position + 1) == this.values.length);
+	}
 }
