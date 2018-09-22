b72caa2d33e456022a1a91ae2cbeea3c0e0c1c5f
burghporter314 <burghporter314@gmail.com>
2018-09-22 19:38:18 -0400

    Updated Functionality / Completed Project

--- src/com/x10host/burghporter314/DatabaseManagementSystem/Main.java
+++ src/com/x10host/burghporter314/DatabaseManagementSystem/Main.java
@@ -10,7 +10,7 @@ import java.util.logging.SimpleFormatter;
 public class Main {
 
 	private static Logger LOGGER = Logger.getLogger(Main.class.getName());
-	private static final String INPUT_FILE_PATH_LOG = "LOG";
+	private static final String INPUT_FILE_PATH_LOG = "dbFiles";
 	private static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) throws SecurityException, IOException {
@@ -27,45 +27,61 @@ public class Main {
 		fh.setFormatter(formatter);
 		
 		int userInput = 0;
+		DatabaseFile file = null;
 		
+		Column[] columns;
+		Record record;
+		
+		System.out.println("1 - Create new Table | 2 - Load Existing Table");
+		userInput = input.nextInt(); input.nextLine();
+		
+		switch(userInput) {
+			case 1:
+				
+				file = new DatabaseFile();
+				String columnData;
+				
+				System.out.print("Enter Table Name: ");
+				file.setName(input.nextLine());
+				
+				while(true) {
+					
+					System.out.print("Enter Column Name and its Length: ");
+					columnData = input.nextLine();
+					
+					if(columnData.isEmpty()) { break; }
+					file.addColumn(new Column(columnData));
+					
+				}
+				
+				file.createTable();
+				LOGGER.info(file.getName());
+
+				break;
+			
+			case 2:
+				
+				System.out.print("Enter File Name: ");
+				file = new DatabaseFile(input.nextLine());
+				System.out.println(file.getName());
+				
+				break;
+				
+		}
+		
+		
+		outer:
 		while(true) {
 			
-			System.out.println("1 - Create New Database | 2 - Add Entry | 3 - Remove Entry | 4 - List Database | 5 - Purge Database | 6 - Exit");
+			columns = file.getColumnArray();
+			record = new Record(columns);
+			
+			System.out.println("1 - Add Entry | 2 - Remove Entry | 3 - List Database | 4 - Exit");
 			userInput = input.nextInt(); input.nextLine();
 			
-			DatabaseFile file;
-			Column[] columns;
-			Record record;
-			
 			switch(userInput) {
+					
 				case 1:
-					
-					file = new DatabaseFile();
-					String columnData;
-					
-					System.out.print("Enter Table Name: ");
-					file.setName(input.nextLine());
-										
-					while(true) {
-						
-						System.out.print("Enter Column Name and its Length: ");
-						columnData = input.nextLine();
-						
-						if(columnData.isEmpty()) { break; }
-						file.addColumn(new Column(columnData));
-						
-					}
-					
-					file.createTable();
-					break;
-					
-				case 2:
-					
-					System.out.print("Enter File Name: ");
-					file = new DatabaseFile(input.nextLine());
-					
-					columns = file.getColumnArray();
-					record = new Record(columns);
 					
 					for(Column column: columns) {
 						System.out.print(column.getName() + ": ");
@@ -74,13 +90,8 @@ public class Main {
 					
 					file.insert(record);
 					break;
-				case 3:
 					
-					System.out.print("Enter File Name: ");
-					file = new DatabaseFile(input.nextLine());
-					
-					columns = file.getColumnArray();
-					record = new Record(columns);
+				case 2:
 					
 					for(Column column: columns) {
 						System.out.print(column.getName() + ": ");
@@ -89,21 +100,19 @@ public class Main {
 					
 					file.remove(record);
 					break;
-				case 4:
 					
-					System.out.print("Enter File Name: ");
-					file = new DatabaseFile(input.nextLine());
+				case 3:
 					
 					file.listFile();
 					break;
-				case 5:
-					//Statements
-					break;
-				case 6:
-					//Statements
-					break;
+					
+				case 4:
+					
+					file.purge();
+					break outer;
+			
 				default:
-					//Statements
+					System.out.println("ERROR: Invalid Input");
 					break;
 			}
 		}
